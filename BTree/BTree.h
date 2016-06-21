7cb3c7adc9548e93d710331a0e64bb13a911874a
rowrowape <zewwws13@gmail.com>
2016-06-21 23:57:12 +0700

    BTree headers ad

--- BTree/BTree.h
+++ BTree/BTree.h
@@ -1,12 +1,39 @@
-//
-// Created by awemath on 21.06.16.
-//
-
 #ifndef B_TREE_BTREE_H
 #define B_TREE_BTREE_H
 
 class BTree {
+public:
+    BTree();
 
+    ~BTree();
+
+    BTree(const BTree &operandTree);
+
+    BTree &operator=(const BTree &operandTree);
+
+    bool searchElement(const BTree &operandTree);
+
+    void insertElement(BTree &operandTree);
+
+    void deleteElement(BTree &operandTree);
+
+    void printTree (const BTree &operandTree);//print tree in ascending order of elements
+
+    void inputElemntFromStream (BTree &operandTree);//input from the stream
+
+    bool isTreeEmpty (const BTree &operandTree);
+
+    void cleanTree (BTree &operandTree); // tree cleaning
+
+    int getOrder (const BTree &operandTree);
+
+    int getSize (const BTree &operandTree);
+
+private:
+    int t; //b-tree order
+    int size;
+    //tree height
+    TreeElem *root; //root of tree
 };
 
 
